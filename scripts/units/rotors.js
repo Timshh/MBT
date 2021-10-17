62c43d9ab2123bf70d561720a7d6f1bda0a42122
Timshh <shirobokihtim@gmail.com>
2021-10-17 22:35:12 +0300

    111

--- scripts/units/rotors.js
+++ scripts/units/rotors.js
@@ -2,7 +2,7 @@ const T1arocket = extend(UnitType, "T1arocket", {
 	draw(unit) {
 		this.super$draw(unit);
 		Draw.rect(
-			"T1arocket-vint",
+			"mbt-T1arocket-rotor",
 			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
 			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
 			Time.time * -15);
