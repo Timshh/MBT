a2345d22a6a816461128cb7246bbee22d1d7beb0
Timshh <shirobokihtim@gmail.com>
2021-10-17 22:44:27 +0300

    1111

--- scripts/units/rotors.js
+++ scripts/units/rotors.js
@@ -3,9 +3,9 @@ const T1arocket = extend(UnitType, "T1arocket", {
 		this.super$draw(unit);
 		Draw.rect(
 			"mbt-T1arocket-rotor",
-			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
-			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
-			Time.time * -15);
+			unit.x + Angles.trnsx(unit.rotation - 180, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 180, 0, 0),
+			Time.time * -20);
 	}
 });
 
