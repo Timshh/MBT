4c65392cd453334768b272208207b24cb79380b9
Timshh <shirobokihtim@gmail.com>
2021-10-18 19:03:46 +0300

    11111

--- scripts/units/rotors.js
+++ scripts/units/rotors.js
@@ -8,5 +8,16 @@ const T1arocket = extend(UnitType, "T1arocket", {
 			Time.time * -20);
 	}
 });
+const T3arocket = extend(UnitType, "T3arocket", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"mbt-T3arocket-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+	}
+});
 
 T1arocket.constructor = () => extend(UnitEntity, {});
+T3arocket.constructor = () => extend(UnitEntity, {});
