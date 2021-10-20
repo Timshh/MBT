aa93bab21fe12fd32fe5e8c86faa5e032bce73c2
Timshh <shirobokihtim@gmail.com>
2021-10-20 17:20:25 +0300

    1111

--- scripts/units/rotors.js
+++ scripts/units/rotors.js
@@ -8,6 +8,16 @@ const T1arocket = extend(UnitType, "T1arocket", {
 			Time.time * -20);
 	}
 });
+const T2arocket = extend(UnitType, "T2arocket", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"mbt-T2arocket-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 180, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 180, 0, 0),
+			Time.time * -20);
+	}
+});
 const T3arocket = extend(UnitType, "T3arocket", {
 	draw(unit) {
 		this.super$draw(unit);
@@ -18,6 +28,18 @@ const T3arocket = extend(UnitType, "T3arocket", {
 			Time.time * -15);
 	}
 });
+const T4arocket = extend(UnitType, "T4arocket", {
+	draw(unit) {
+		this.super$draw(unit);
+		Draw.rect(
+			"mbt-T4arocket-rotor",
+			unit.x + Angles.trnsx(unit.rotation - 90, 0, 0),
+			unit.y + Angles.trnsy(unit.rotation - 90, 0, 0),
+			Time.time * -15);
+	}
+});
 
 T1arocket.constructor = () => extend(UnitEntity, {});
+T2arocket.constructor = () => extend(UnitEntity, {});
 T3arocket.constructor = () => extend(UnitEntity, {});
+T4arocket.constructor = () => extend(UnitEntity, {});
